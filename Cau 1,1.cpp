5f0586f75e2240b71f63702c04fc9a72fc6b7a93
ghieu123 <giahieuc5@gmail.com>
2024-08-05 15:39:49 +0700

    Cau 1,1

---
+++ Cau 1,1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// Hàm đệ quy và khử đệ quy cho S(n) = 1 + 2 + 3 + ... + n
+int S1_dequy(int n) {
+    if (n == 1) return 1;
+    return n + S1_dequy(n - 1);
+}
+
+int S1_khudequy(int n) {
+    return n * (n + 1) / 2;
+}
+
+int main() {
+    int n;
+    cout << "Nhap n: ";
+    cin >> n;
+
+    cout << "S1 de quy: " << S1_dequy(n) << endl;
+    cout << "S1 khu de quy: " << S1_khudequy(n) << endl;
+
+    return 0;
+}
