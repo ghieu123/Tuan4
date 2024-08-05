7d81f0a64463bb0d14d5731008d4c63e782f1f29
ghieu123 <giahieuc5@gmail.com>
2024-08-05 15:53:40 +0700

    Cau 1,2

---
+++ Cau 1,2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// Hàm đệ quy và khử đệ quy cho S(n) = 1/2 + 2/3 + ... + n/(n+1)
+double S2_dequy(int n) {
+    if (n == 1) return 1.0 / 2;
+    return (double)n / (n + 1) + S2_dequy(n - 1);
+}
+
+double S2_khudequy(int n) {
+    double sum = 0;
+    for (int i = 1; i <= n; ++i) {
+        sum += (double)i / (i + 1);
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+    cout << "Nhap n: ";
+    cin >> n;
+
+    cout << "S2 de quy: " << S2_dequy(n) << endl;
+    cout << "S2 khu de quy: " << S2_khudequy(n) << endl;
+
+    return 0;
+}
